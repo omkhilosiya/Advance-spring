5cc886e5c7cdaaf6edd071524bd4cd7a9872ccc9
omjai <omjai@maabalvikrupa>
2025-09-01 11:17:58 +0530

    intial setup done

---
+++ SpringRevision/src/main/java/com/example/SpringRevision/SpringRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRevisionApplication.class, args);
+	}
+
+}
---
+++ SpringRevision/src/main/java/com/example/SpringRevision/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.SpringRevision.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	
+	@GetMapping("/welcome")
+	public String welcomeMessage() {	
+		return "hello its working";
+	}
+
+}
