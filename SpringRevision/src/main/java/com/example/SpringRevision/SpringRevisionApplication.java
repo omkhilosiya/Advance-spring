4f684a8d3d14b000cdca50edecab64a61766f72c
omjai <omjai@maabalvikrupa>
2025-10-31 17:48:40 +0530

    Revert "git new changes"
    This reverts commit 23c8f317246f04c2ea013fd816406c462bbf1a1e.

--- SpringRevision/src/main/java/com/example/SpringRevision/SpringRevisionApplication.java
+++ SpringRevision/src/main/java/com/example/SpringRevision/SpringRevisionApplication.java
@@ -3,7 +3,7 @@ package com.example.SpringRevision;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example.SpringRevision", "repository", "service"})
+@SpringBootApplication
 public class SpringRevisionApplication {
 
 	public static void main(String[] args) {
